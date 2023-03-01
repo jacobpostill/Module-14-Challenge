b36225e5cab1c529320bcb98cb5942e066b42af4
Jacob Postill <postilljacob@gmail.com>
2023-02-28 23:12:35 -0500

    fixing bugs

--- public/js/post.js
+++ public/js/post.js
@@ -1,19 +1,20 @@
 const postFormHandler = async (event) => {
   event.preventDefault();
-  const postDetail = document.getElementById('#post-detail').value.trim();
-  const postImgurl = document.getElementById('#post-imgurl').value.trim();
-  const postId = document.getElementById('#postid').value.trim();
+  const postSubject = document.getElementById('post-subject').value.trim();
+  const postDetail = document.getElementById('post-detail').value.trim();
+  const postImgurl = document.getElementById('post-imgurl').value.trim();
+  const postId = document.getElementById('postid').value.trim();
 
   let fetchUrl = "", postMethod = "";
   if (postSubject && postImgurl && postDetail) {
     if (postId != "")
     {
-      fetchUrl = "/api/post/" + postId;
+      fetchUrl = "/api/posts/" + postId;
       postMethod = "PUT";
     }
     else
     {
-      fetchUrl = "/api/post";
+      fetchUrl = "/api/posts";
       postMethod = "POST";
     }
     const response = await fetch(fetchUrl, {
@@ -26,27 +27,26 @@ const postFormHandler = async (event) => {
     } else {
       const isJson = response.headers.get('content-type')?.includes('application/json');
       const data = isJson ? await response.json() : null;   
-    }}};
+    }
+  }
+};
 
 const postDelete = async () => {
-
   const postId = document.querySelector('#postid').value.trim();
+  let fetchUrl = "/api/posts/" + postId;
+  let postMethod = "DELETE";
 
+  const response = await fetch(fetchUrl, {
+    method: postMethod,
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    const isJson = response.headers.get('content-type')?.includes('application/json');
+    const data = isJson ? await response.json() : null; 
+  }
+};
 
-  let fetchUrl = "", postMethod = "";
-  fetchUrl = "/api/posts/" + postId;
-  postMethod = "DELETE";
-
-    const response = await fetch(fetchUrl, {
-      method: postMethod,
-      headers: { 'Content-Type': 'application/json' },
-    });
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      const isJson = response.headers.get('content-type')?.includes('application/json');
-      const data = isJson ? await response.json() : null; 
-    }};
-
-document.getElementById('#post-delete').addEventListener('click', postDelete);
-document.getElementById('#post-submit').addEventListener('click', postFormHandler);
+document.getElementById('post-delete').addEventListener('click', postDelete);
+document.getElementById('post-submit').addEventListener('click', postFormHandler);
